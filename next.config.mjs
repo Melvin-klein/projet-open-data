aca8bdcaf0897fb57eb8132bb2f4c8c9cb753dc6
Melvin-klein <melvine.nargeot@gmail.com>
2025-02-15 22:08:33 +0100

    Try to fix deployement

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/projet-open-data",
-    assetPrefix: "/projet-open-data/",
+    output: "export", // Assure un export statique pour GitHub Pages
+    basePath: "/projet-open-data", // Définit le bon chemin de base
+    assetPrefix: "/projet-open-data/", // Définit le bon préfixe pour les assets
+    images: {
+        unoptimized: true, // Évite les erreurs liées aux optimisations d'images
+    },
 };
 
 export default nextConfig;
